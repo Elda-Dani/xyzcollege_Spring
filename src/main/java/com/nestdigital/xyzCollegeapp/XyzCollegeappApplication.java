60e093671fd8303df2dfbc14a08d65f80a43026a
Elda-Dani <eldadani2001@gmail.com>
2022-10-19 08:55:26 +0530

    add

---
+++ src/main/java/com/nestdigital/xyzCollegeapp/XyzCollegeappApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzCollegeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzCollegeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzCollegeappApplication.class, args);
+	}
+
+}
